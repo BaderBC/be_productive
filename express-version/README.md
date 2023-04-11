1553561226cbc51d2ca09de1faf94f8a55fb7f67
Bartłomiej Strama <kontakt@bstrama.com>
2023-04-12 00:02:06 +0200

    codebase changes

--- express-version/README.md
+++
@@ -1,2 +0,0 @@
-# be_productive
-I'm currently creating a website to help improve my productivity. This project is created for my own purpose.
--- express-version/services/postgres.service.js
+++
@@ -1,8 +0,0 @@
-const pgp = require('pg-promise')();
-
-
-const {
-    DATABASE_URL = "postgresql://localhost/be_productive"
-} = process.env;
-
-global.db = pgp(DATABASE_URL);
