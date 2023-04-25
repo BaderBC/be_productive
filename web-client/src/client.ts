7101eeb0406f1449ddfeed2d65b82c7d40bcc375
Bartłomiej Strama <kontakt@bstrama.com>
2023-04-25 21:30:08 +0200

    graphQl svelte (client) support added

---
+++ web-client/src/client.ts
@@ -0,0 +1,8 @@
+import { HoudiniClient } from '$houdini';
+
+export default new HoudiniClient({
+    url: 'http://localhost:3000/graphql',
+    fetchParams() { 
+        return { credentials: 'include' };
+    }
+});
