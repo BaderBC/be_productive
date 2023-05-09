8b10fdbcf77d9b8704dc8643458019ecda3ff6fe
Bartłomiej Strama <kontakt@bstrama.com>
2023-05-09 22:09:16 +0200

    fixed docker ARGs and cleaned up docker

--- web-client/src/client.ts
+++ web-client/src/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_URL,
+  uri: import.meta.env.VITE_BACKEND_URL + `/graphql`,
   credentials: `include`,
   cache: new InMemoryCache(),
 });
